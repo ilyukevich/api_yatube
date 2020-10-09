84dc13fa132d6347ef351a91fd042b7363c16ee8
ilyukevich <9009901@gmail.com>
2020-10-09 18:14:26 +0300

    09/10/2020 - review 2

--- api/serializers.py
+++ api/serializers.py
@@ -4,20 +4,20 @@ from rest_framework.relations import SlugRelatedField
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """Class PostSerializer for Post"""
 
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = ('id', 'text', 'author', 'image', 'pub_date')
-        read_only_fields = ['author']
+        fields = '__all__'
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """Class CommentSerializer for Comment"""
 
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = ('id', 'text', 'author', 'post', 'created')
-        read_only_fields = ['author']
+        fields = '__all__'
         model = Comment
--- api/views.py
+++ api/views.py
@@ -1,39 +1,37 @@
 from rest_framework import status, viewsets
-from rest_framework.response import Response
 from posts.models import Post, Comment
 from django.shortcuts import get_object_or_404
 from .serializers import CommentSerializer, PostSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import permissions
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+from .permissions import IsOwnerOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    """ViewSets for Post.
+    overridden 'perform_create' method."""
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
+        """create method"""
         serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """ViewSets for Comment.
+    overridden 'get_queryset', 'perform_create method'."""
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self):
+        """Get all comments"""
         post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
         return post.comments.all()
 
     def perform_create(self, serializer):
+        """create method"""
         serializer.save(author=self.request.user)
